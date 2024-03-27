ff7e348569452d8c11600d3a458f8f66c6885abc
Mark <mrkcarlos576@gmail.com>
2024-03-27 12:21:23 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    console.log('This one is on me!')
+  } else if (distance > 400 && distance <= 2000) {
+    console.log('That will be twenty bucks.')
+  } else if (distance > 2000) {
+    console.log('I will gladly take your thirty bucks.')
+  } else if (distance > 2500) {
+    console.log('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+ let message = destination === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+ console.log(message)
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous': 
+      console.log('Thank you so much.')
+    break;
+    case 'not as generous':
+      console.log('Thank you.')
+    break;
+    default:
+      console.log('Bye.')
+  }
 }
+
+
+
+
