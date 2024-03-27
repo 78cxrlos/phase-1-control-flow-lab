e08a00a1db64dba276174471c82480bdc106b5b5
Mark <mrkcarlos576@gmail.com>
2024-03-27 15:11:00 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 function scuberGreetingForFeet(distance){
   // Write your code here!
   if (distance <= 400){
-    console.log('This one is on me!')
+    return 'This one is on me!'
   } else if (distance > 400 && distance <= 2000) {
-    console.log('That will be twenty bucks.')
-  } else if (distance > 2000) {
-    console.log('I will gladly take your thirty bucks.')
-  } else if (distance > 2500) {
-    console.log('No can do.')
+    return 'That will be twenty bucks.'
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } 
+  else {
+    return 'No can do.'
   }
 }
 
-function ternaryCheckCity(destination){
+console.log(distance(250))
+
+function ternaryCheckCity(city){
   // Write your code here!
- let message = destination === 'NYC' ? 'Ok, sounds good.' : 'No go.'
- console.log(message)
+ let message = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+ return message
 }
+
+console.log(city('NYC'))
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
   switch (tip) {
     case 'generous': 
-      console.log('Thank you so much.')
+      return 'Thank you so much.'
     break;
     case 'not as generous':
-      console.log('Thank you.')
+      return 'Thank you.'
     break;
     default:
-      console.log('Bye.')
+      return 'Bye.'
   }
 }
+
+console.log(tip('generous'))
 
 
 
